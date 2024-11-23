19dc6519281fdc7b45d84d00ced442cdc63290f6
SoraKang <70902065+sora9z@users.noreply.github.com>
2024-11-23 16:02:47 +0900

    [style] 프로젝트 구조 변경 - database 설정 구성 변경, user모델 위치 이동
    - user 모델 위치 수정

---
+++ app/common/database/database.py
@@ -0,0 +1,51 @@
+# about engine: https://docs.sqlalchemy.org/en/20/core/engines.html
+# postgresql dialet : https://docs.sqlalchemy.org/en/20/dialects/postgresql.html
+# about session: https://docs.sqlalchemy.org/en/20/orm/session_basics.html#what-does-the-session-do
+# async engine : https://docs.sqlalchemy.org/en/20/orm/extensions/asyncio.html#sqlalchemy.ext.asyncio.AsyncEngine
+# async session : https://docs.sqlalchemy.org/en/20/orm/extensions/asyncio.html#sqlalchemy.ext.asyncio.AsyncSession
+
+import logging
+from asyncio import current_task
+from contextlib import asynccontextmanager
+from typing import AsyncGenerator
+
+import psycopg
+from sqlalchemy import URL
+from sqlalchemy.ext.asyncio import AsyncSession, async_scoped_session, async_sessionmaker, create_async_engine
+
+logger = logging.getLogger(__name__)
+
+
+class Database:
+    def __init__(self, username: str, password: str, host: str, database: str) -> None:
+
+        url_object = URL.create(
+            "postgresql+psycopg",
+            username=username,
+            password=password,
+            host=host,
+            database=database,
+        )  # dialect+driver://username:password@host:port/database
+
+        self.async_engine = create_async_engine(url_object, pool_pre_ping=True, echo=True)
+
+        self.async_session = async_scoped_session(
+            async_sessionmaker(
+                self.async_engine,
+                expire_on_commit=False,
+                autoflush=False,
+                future=True,
+            ),
+            scopefunc=current_task,
+        )
+
+    @asynccontextmanager
+    async def get_session(self) -> AsyncGenerator[AsyncSession, None]:
+        session: AsyncSession = self.async_session()
+        try:
+            yield session
+        except psycopg.IntegrityError as exception:
+            logger.error("Session rollback : %s", exception)
+            await session.rollback()
+        finally:
+            await session.close()
---
+++ app/common/database/models/user.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.dialects.postgresql import ENUM
+
+from app.common.constants import UserType
+from app.common.database.models.base import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    type = Column(
+        ENUM(UserType, name="user_type", create_type=True),
+        default=UserType.USER.value,
+    )
