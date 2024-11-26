49f8915bad2c36552f73d5ab563e264f46650cab
SoraKang <70902065+sora9z@users.noreply.github.com>
2024-11-27 01:05:06 +0900

    [refact] reservation model 컬럼 수정
    - 불필요 컬럼 제거
    - start-time, end-time type 변경 : datatime -> time
    - 마이그레이션 정리

--- app/common/database/models/reservation.py
+++ app/common/database/models/reservation.py
@@ -1,5 +1,5 @@
-from sqlalchemy import CheckConstraint, Column, Date, DateTime, ForeignKey, Index, Integer, Table
-from sqlalchemy.dialects.postgresql import ENUM, TSTZRANGE
+from sqlalchemy import CheckConstraint, Column, Date, ForeignKey, Index, Integer, Table, Time
+from sqlalchemy.dialects.postgresql import ENUM
 from sqlalchemy.orm import relationship
 
 from app.common.constants import ReservationStatus
@@ -21,18 +21,15 @@ class Reservation(Base):
     id = Column(Integer, primary_key=True, index=True)
     user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
     exam_date = Column(Date, nullable=False)
-    exam_start_date = Column(DateTime, nullable=False)
-    exam_end_date = Column(DateTime, nullable=False)
+    exam_start_time = Column(Time, nullable=False)
+    exam_end_time = Column(Time, nullable=False)
     applicants = Column(Integer, nullable=False)
     status = Column(ENUM(ReservationStatus, name="reservation_status"), default=ReservationStatus.PENDING.value)
-
-    time_range = Column(TSTZRANGE, nullable=False)  # 시험 시작 시간 ~ 종료 시간 검색을 위한 컬럼
 
     user = relationship("User", back_populates="reservations")
     slots = relationship("Slot", secondary=reservation_slots, back_populates="reservations")
 
     __table_args__ = (
-        Index("idx_reservations_time_range", "time_range", postgresql_using="gist"),
         Index("idx_reservations_exam_date", "exam_date"),
-        CheckConstraint("exam_end_date > exam_start_date", name="check_exam_time_valid"),
+        CheckConstraint("exam_end_time > exam_start_time", name="check_exam_time_valid"),
     )
